d9d917084e73d2a7bd7d3871deefbb09736f5efa
devoris <oussam92.ing@gmail.com>
2021-11-09 14:17:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import image1 from "./images/pic1.png";
+import image2 from "./images/pic2.png";
+import video1 from "./video/short-video.mp4";
+import "./css/style.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Col">
+        <h1 className="text1">Your name here</h1>
+        <br />
+        <div className="imag">
+          <img src={image1} alt="image1" />
+          <br />
+          <img src={image2} alt="image2" />
+        </div>
+      </div>
+      <div className="vid">
+        <video width="320" height="240" controls>
+          <source src={video1} />
+        </video>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
